d32cf81d2ed6bba33a48e9f5a2b6dec566a496dc
stortrut <35192303+stortrut@users.noreply.github.com>
2025-09-17 16:33:40 +0200

    Add files via upload

---
+++ generate-pages.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+const articlesFolder = path.join(__dirname, 'articles');
+const outputFile = path.join(articlesFolder, 'pages.json');
+
+function walkDir(dir, callback) {
+  fs.readdirSync(dir).forEach(file => {
+    const fullPath = path.join(dir, file);
+    const stat = fs.statSync(fullPath);
+    if (stat.isDirectory()) {
+      walkDir(fullPath, callback); // recursive call
+    } else {
+      callback(fullPath);
+    }
+  });
+}
+
+const pages = [];
+
+walkDir(articlesFolder, (filePath) => {
+  const relativePath = path.relative(articlesFolder, filePath);
+  if (
+    filePath.endsWith('.html') &&
+    path.basename(filePath) !== 'index.html'
+  ) {
+    const name = path.basename(filePath, '.html')
+      .replace(/[-_]/g, ' ')
+      .replace(/\b\w/g, c => c.toUpperCase());
+
+    pages.push({
+      name,
+      url: `/articles/${relativePath.replace(/\\/g, '/')}` // Normalize for web URLs
+    });
+  }
+});
+
+fs.writeFile(outputFile, JSON.stringify(pages, null, 2), (err) => {
+  if (err) {
+    console.error('Error writing pages.json:', err);
+  } else {
+    console.log('✅ pages.json created with', pages.length, 'articles.');
+  }
+});
